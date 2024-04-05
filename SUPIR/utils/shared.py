829004967c6d60f99273f123184f83dc9fec2993
Hughes, Lance <lance.hughes@sap.com>
2024-04-04 17:54:48 -0700

    moved files around

--- SUPIR/utils/shared.py
+++
@@ -1,8 +0,0 @@
-import types
-
-opts = pipe_encode = types.SimpleNamespace() 
-opts.fp8_storage=False
-opts.opt_channelslast=False
-opts.half_mode = False
-opts.upcast_sampling = False
-opts.fast_load_sd = False
